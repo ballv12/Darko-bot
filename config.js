ad1831699aa7ab734f73605a0c3fa47a85eaa55b
ball <ballcruzv@gmail.com>
2025-04-18 00:06:42 -0700

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ module.exports = {
     ],
     songStatus: true
   },
-  spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
-  spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
+  spotifyClientId: "c5d16f8f85b24fbcb0329c3dd32c1a58",
+  spotifyClientSecret: "189e59e7043941b69c3c7ae87de81131",
 }
